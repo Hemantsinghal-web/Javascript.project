3c8dffff448c0bc90e2ff99aceece36b5936036f
Hemantsinghal-web <singhal2005hemant@gmail.com>
2025-02-07 22:55:08 +0530

    07/02

---
+++ index.js
@@ -0,0 +1,21 @@
+// function buttonn()
+// {
+//     let btn = document.querySelector('.button')
+//     btn.style.display="block"
+//     // alert("Fas")
+// }
+
+function addnum()
+{
+    let number = document.querySelector('#number').value
+    number.innerHTML =number+1;
+    alert("sfsf")
+}
+
+
+function sub()
+{
+    let number = document.querySelector('#number').value
+    number.innerHTML =number-1;
+    alert("sfsf")
+}
